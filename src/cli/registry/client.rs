b7eebf0411c6362dff1afa5b61f2d45e88251d9d
Flavio Del Grosso <flaviodelgrosso@live.it>
2024-10-13 21:45:32 +0200

    refactor: move is_package_updatable in versions module

--- src/cli/registry/client.rs
+++ src/cli/registry/client.rs
@@ -1,5 +1,4 @@
 use anyhow::Result;
-use nodejs_semver::{Range, Version};
 use reqwest::header::{HeaderMap, HeaderValue, ACCEPT};
 use reqwest::{Client, ClientBuilder};
 use url::Url;
@@ -9,7 +8,7 @@ use super::{RegistryClientOptions, RegistryError};
 use crate::cli::{
   flags::Flags,
   package_info::PackageInfo,
-  versions::{normalize_version, DistTags, VersionTarget},
+  versions::{is_package_updatable, DistTags, VersionTarget},
 };
 
 #[derive(Debug)]
@@ -45,7 +44,7 @@ impl RegistryClient {
   ) -> Result<Option<PackageInfo>> {
     let latest_version = self.fetch_package_version(name, flags).await?;
 
-    if Self::is_package_updatable(current_version, &latest_version, flags)? {
+    if is_package_updatable(current_version, &latest_version, flags)? {
       Ok(Some(PackageInfo {
         pkg_name: name.to_string(),
         current_version: current_version.to_string(),
@@ -98,40 +97,5 @@ impl RegistryClient {
         .unwrap_or(dist_tags.latest),
       _ => dist_tags.latest,
     }
-  }
-
-  pub fn is_package_updatable(
-    current_version: &str,
-    latest_version: &str,
-    flags: &Flags,
-  ) -> Result<bool> {
-    let curr_ver_range = Range::parse(current_version)?;
-    let latest_ver = Version::parse(latest_version)?;
-    let current_ver = Version::parse(normalize_version(current_version))?;
-
-    // Check if an update can be made based on the flag
-    let can_update = match flags.target {
-      Some(VersionTarget::Latest) => {
-        // Check if latest version is within the allowed range and newer
-        if curr_ver_range.satisfies(&latest_ver) {
-          latest_ver > current_ver
-        } else {
-          !current_ver.is_prerelease() // Do not allow prereleases if range is invalid
-        }
-      }
-      Some(VersionTarget::Major) => latest_ver.major > current_ver.major,
-      Some(VersionTarget::Minor) => {
-        latest_ver.major == current_ver.major && latest_ver.minor > current_ver.minor
-      }
-      Some(VersionTarget::Patch) => {
-        latest_ver.major == current_ver.major
-          && latest_ver.minor == current_ver.minor
-          && latest_ver.patch > current_ver.patch
-      }
-      Some(VersionTarget::Pre) => latest_ver.is_prerelease() && latest_ver > current_ver,
-      None => false,
-    };
-
-    Ok(can_update)
   }
 }
--- src/cli/versions/utils.rs
+++ src/cli/versions/utils.rs
@@ -1,3 +1,45 @@
+use anyhow::Result;
+use nodejs_semver::{Range, Version};
+
+use crate::cli::flags::Flags;
+
+use super::VersionTarget;
+
 pub fn normalize_version(version: &str) -> &str {
   version.trim_start_matches(|c: char| !c.is_numeric())
+}
+
+pub fn is_package_updatable(
+  current_version: &str,
+  latest_version: &str,
+  flags: &Flags,
+) -> Result<bool> {
+  let curr_ver_range = Range::parse(current_version)?;
+  let latest_ver = Version::parse(latest_version)?;
+  let current_ver = Version::parse(normalize_version(current_version))?;
+
+  // Check if an update can be made based on the flag
+  let can_update = match flags.target {
+    Some(VersionTarget::Latest) => {
+      // Check if latest version is within the allowed range and newer
+      if curr_ver_range.satisfies(&latest_ver) {
+        latest_ver > current_ver
+      } else {
+        !current_ver.is_prerelease() // Do not allow prereleases if range is invalid
+      }
+    }
+    Some(VersionTarget::Major) => latest_ver.major > current_ver.major,
+    Some(VersionTarget::Minor) => {
+      latest_ver.major == current_ver.major && latest_ver.minor > current_ver.minor
+    }
+    Some(VersionTarget::Patch) => {
+      latest_ver.major == current_ver.major
+        && latest_ver.minor == current_ver.minor
+        && latest_ver.patch > current_ver.patch
+    }
+    Some(VersionTarget::Pre) => latest_ver.is_prerelease() && latest_ver > current_ver,
+    None => false,
+  };
+
+  Ok(can_update)
 }
